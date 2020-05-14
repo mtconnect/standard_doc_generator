e1f736b193ee20868fca2dbbb5601760d7dd5aa7
Shaurabh Singh <ssingh@amtonline.org>
2020-05-14 16:53:12 -0400

    Added methods to gen section intros

--- src/create_documentation.rb
+++ src/create_documentation.rb
@@ -4,27 +4,79 @@ require 'latex_model'
 LatexModel.skip_models = SkipModels
 LatexModel.new(RootModel).find_definitions
 
+$dataitem_types = {}
+
+LatexModel.models['SampleTypes'].types.each do |type|
+	$dataitem_types[type.name] = []
+	type.children.each do |subtype|
+		$dataitem_types[type.name] << subtype
+	end
+end
+
+LatexModel.models['EventTypes'].types.each do |type|
+	$dataitem_types[type.name] = []
+	type.children.each do |subtype|
+		$dataitem_types[type.name] << subtype
+	end
+end
+
+def get_section_documentation(root_model,section_package_name, section_name)
+	section_model = root_model.at("//packagedElement[@name='#{section_package_name}']")
+	if section_model and section_model.at("//*[@body='#{section_name}']")
+		return section_model.at("//*[@body='#{section_name}']").ownedComment['body']
+	else
+		return ""
+	end
+end
+
+
+def section_intro(f,root_model,section_package_name, section_name)
+  f.puts "\\section{#{section_name}}\n\\label{sec:#{section_name}}\n"
+  f.puts get_section_documentation(root_model, section_package_name, section_name)
+end
+
+
 $logger.info "\nGenerating Devices LaTex to #{DocumentFile['Device']}"
+$enums = []
 File.open(DocumentFile['Device'], 'w') do |f|
   f.puts "% Generated #{Time.now}"
   
-  f.puts "\\section{Devices Information Model}\n"
+  section_intro(f,RootModel, "Supporting Documents", "Devices Information Model")
+
+  section_intro(f,RootModel, DeviceModels[1], "Structural Elements for MTConnectDevices")
   LatexModel.directory = Directory['Device']
   DeviceModels.each do |m|
     LatexModel.generate_latex(f, m)
   end
   
-  f.puts "\\section{Configuration}\n"
+  section_intro(f,RootModel, CompositionModels[0], CompositionModels[0])
+  CompositionModels.each do |c|
+    LatexModel.generate_latex(f, c)
+  end
+  
+  section_intro(f,RootModel, DataItemModels[0], DataItemModels[0])
+  DataItemModels.each do |c|
+    LatexModel.generate_latex(f, c)
+  end
+  
+  section_intro(f,RootModel, ReferenceModels[0], ReferenceModels[0])
+  ReferenceModels.each do |c|
+    LatexModel.generate_latex(f, c)
+  end
+
+  section_intro(f,RootModel, ConfigurationModels[0], ConfigurationModels[0])
   ConfigurationModels.each do |c|
     LatexModel.generate_latex(f, c)
   end
   
-  f.puts "\\section{MTConnect Profile}\n"
+  section_intro(f,RootModel, ProfileModels[0], "MTConnect Profile")
   ProfileModels.each do |p|
     LatexModel.generate_latex(f, p)
   end
 end
 
+
+=begin
 $logger.info "\nGenerating Observations LaTex to #{DocumentFile['Observation']}"
 File.open(DocumentFile['Observation'], 'w') do |f|
   f.puts "% Generated #{Time.now}"
@@ -37,14 +89,25 @@ File.open(DocumentFile['Observation'], 'w') do |f|
 end
 
 
-$logger.info "\nGenerating Assets LaTex to #{DocumentFile['AssetModel']}"
-File.open(DocumentFile['AssetModel'], 'w') do |f|
+$logger.info "\nGenerating Assets LaTex to #{DocumentFile['Asset']}"
+File.open(DocumentFile['Asset'], 'w') do |f|
   f.puts "% Generated #{Time.now}"
   
-  f.puts "\\section{Assets Model}\n"
-  LatexModel.directory = Directory['AssetModel']
+  f.puts "\\section{Assets}\n"
+  LatexModel.directory = Directory['Asset']
   AssetModels.each do |m|
 	puts "Generating for #{m}"
     LatexModel.generate_latex(f, m)
   end
 end
+
+$logger.info "\nGenerating Glossary LaTex to #{DocumentFile['Glossary']}"
+File.open(DocumentFile['Glossary'], 'w') do |f|
+  f.puts "% Generated #{Time.now}"
+  
+  LatexModel.directory = Directory['Glossary']
+  Glossary.each do |m|
+    LatexModel.generate_glossary(f, m)
+  end
+end
+=end
